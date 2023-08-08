cff70b491e43aff74539fa89f9c48a966c44eabc
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-08-08 18:03:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const user = {id: 12, name: Shuchy, job: Student};
+// console.log(user);
+
+const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+const object = JSON.parse(stringified);
+// console.log(object);
+
+const company = {
+        name : "facebook",
+        ceo: {
+                name: "Mark",
+                id: 2,
+                food: "Susie"
+
+        }
+        web: {
+                work: "Website Development",
+                employee: 25,
+                framework: "React Js",
+                tech: {
+                        first: 'html',
+                        second: 'css',
+                        third: 'js'
+                }
+        }
+}
+
+const companyStringfied = JSON.stringify(company);
+console.log(companyStringfied);
+
+const convert = JSON.parse(companyStringfied);
+console.log(convert);
+
+
